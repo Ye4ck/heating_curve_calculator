54c9fbe9490b398c8108d684ce5b9a4215e9bf29
Ye4ck <168309238+Ye4ck@users.noreply.github.com>
2026-02-11 22:46:26 +0100

    Update select.py

--- custom_components/heating_curve/select.py
+++ custom_components/heating_curve/select.py
@@ -7,6 +7,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.restore_state import RestoreEntity
 
 from .const import (
     DOMAIN,
@@ -30,7 +31,7 @@ async def async_setup_entry(
     async_add_entities([select], True)
 
 
-class HeatingCurveSelect(SelectEntity):
+class HeatingCurveSelect(SelectEntity, RestoreEntity):
     """Representation of a Heating Curve calculation mode selector."""
 
     _attr_has_entity_name = True
@@ -59,14 +60,34 @@ class HeatingCurveSelect(SelectEntity):
             "name": device_name,
             "manufacturer": "Custom",
             "model": "Heating Curve Calculator",
-            "sw_version": "2.0.0",
+            "sw_version": "2.1.0",
         }
         
-        # Get initial value
-        entry_data = hass.data[DOMAIN][config_entry.entry_id]
-        self._attr_current_option = entry_data["state"].get(
-            "calculation_mode", DEFAULT_CALCULATION_MODE
-        )
+        # Set default (will be overridden in async_added_to_hass)
+        self._attr_current_option = DEFAULT_CALCULATION_MODE
+
+    async def async_added_to_hass(self) -> None:
+        """Restore last known value when entity is added to hass."""
+        await super().async_added_to_hass()
+
+        # Try to restore the previous state
+        last_state = await self.async_get_last_state()
+        if (
+            last_state is not None
+            and last_state.state not in (None, "unknown", "unavailable")
+        ):
+            restored = last_state.state
+            if restored in self._attr_options:
+                self._attr_current_option = restored
+                _LOGGER.debug("Restored calculation_mode to %s", restored)
+            else:
+                _LOGGER.warning(
+                    "Restored state '%s' not in options, using default", restored
+                )
+
+        # Sync the restored (or default) value into hass.data
+        entry_data = self.hass.data[DOMAIN][self._config_entry.entry_id]
+        entry_data["state"]["calculation_mode"] = self._attr_current_option
 
     async def async_select_option(self, option: str) -> None:
         """Change the selected option."""
@@ -100,4 +121,3 @@ class HeatingCurveSelect(SelectEntity):
     def translation_key(self) -> str:
         """Return the translation key."""
         return "calculation_mode"
-
