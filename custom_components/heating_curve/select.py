5819ce7c0c4e78796acb72f4c0833b7e1b31bf04
Ye4ck <168309238+Ye4ck@users.noreply.github.com>
2026-09-02 21:05:23 +0200

    Refactor select.py to clean up comments and code

--- custom_components/heating_curve/select.py
+++ custom_components/heating_curve/select.py
@@ -47,14 +47,9 @@ class HeatingCurveSelect(SelectEntity, RestoreEntity):
         """Initialize the select entity."""
         self.hass = hass
         self._config_entry = config_entry
-        
-        # Get device name
+
         device_name = config_entry.data.get(CONF_NAME, "Heating Curve")
-        
-        # Generate unique_id
         self._attr_unique_id = f"{config_entry.entry_id}_calculation_mode"
-        
-        # Device info
         self._attr_device_info = {
             "identifiers": {(DOMAIN, config_entry.entry_id)},
             "name": device_name,
@@ -62,15 +57,14 @@ class HeatingCurveSelect(SelectEntity, RestoreEntity):
             "model": "Heating Curve Calculator",
             "sw_version": SW_VERSION,
         }
-        
-        # Set default (will be overridden in async_added_to_hass)
+
+        # Overridden with the restored value in async_added_to_hass, if any
         self._attr_current_option = DEFAULT_CALCULATION_MODE
 
     async def async_added_to_hass(self) -> None:
         """Restore last known value when entity is added to hass."""
         await super().async_added_to_hass()
 
-        # Try to restore the previous state
         last_state = await self.async_get_last_state()
         if (
             last_state is not None
@@ -85,21 +79,16 @@ class HeatingCurveSelect(SelectEntity, RestoreEntity):
                     "Restored state '%s' not in options, using default", restored
                 )
 
-        # Sync the restored (or default) value into hass.data
         entry_data = self.hass.data[DOMAIN][self._config_entry.entry_id]
         entry_data["state"]["calculation_mode"] = self._attr_current_option
 
     async def async_select_option(self, option: str) -> None:
         """Change the selected option."""
-        # Update in hass.data
         entry_data = self.hass.data[DOMAIN][self._config_entry.entry_id]
         entry_data["state"]["calculation_mode"] = option
         self._attr_current_option = option
-        
-        # Notify sensor to update
         self.async_write_ha_state()
-        
-        # Signal other entities to update
+
         self.hass.bus.async_fire(
             f"{DOMAIN}_parameter_changed",
             {
@@ -111,7 +100,6 @@ class HeatingCurveSelect(SelectEntity, RestoreEntity):
 
     async def async_update(self) -> None:
         """Update the entity."""
-        # Get current value from state
         entry_data = self.hass.data[DOMAIN][self._config_entry.entry_id]
         self._attr_current_option = entry_data["state"].get(
             "calculation_mode", DEFAULT_CALCULATION_MODE
